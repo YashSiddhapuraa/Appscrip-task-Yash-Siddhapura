c634ec2851aa6d84ce1f28cbbc647600c671d431
Yash Siddhapuraa <yashkumarsiddhapura@gmail.com>
2024-04-21 12:48:28 +0530

    add seo and favicon icon

--- src/app/collection/layout.js
+++ src/app/collection/layout.js
@@ -2,6 +2,11 @@ import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import React from "react";
 
+export const metadata = {
+  title: "Shop",
+  description: "E-commerce Yash Siddhapura Website",
+};
+
 const Layout = ({ children }) => {
   return (
     <div>
